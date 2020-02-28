90c61c8a56a1f5d55e698ccfff6a3353c1f0b342
nel850 <nelsonmunyua8@gmail.com>
2020-02-28 11:17:27 +0300

    second commit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_movies
+from .request import get_movies,get_movie
 
 # decorators
 @app.route('/')
@@ -23,4 +23,7 @@ def movie(movie_id):
     """
     view movie page function that returns the the movie details page and its data
     """
-    return render_template('movie.html', id = movie_id)
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html', title = title, movie =movie)
